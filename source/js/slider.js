b4db8102c4230ecdb6cfac4aa2fd8ba105ff303d
Boris Larchenko <b.larchenko@pulsarfour.com>
2018-10-14 18:24:04 +0300

    add footer to site and work on footer

--- source/js/slider.js
+++ source/js/slider.js
@@ -22,18 +22,60 @@
     dots[slideIndex-1].className += " slider__switch--active";
   }
 
-  var bulet = document.querySelectorAll(".slider__switch");
+  var slider = document.querySelector(".slider");
+  var bulet = slider.querySelectorAll(".slider__switch");
 
-  bulet[0].onclick = function () {
-    currentDiv(1);
-  };
-  bulet[1].onclick = function () {
-    currentDiv(2);
+  for (let i = 0; i < bulet.length; i++) {
+    bulet[i].onclick = function () {
+      currentDiv(i+1);
+    };
+  }
+
+  var slideIndex2 = 1;
+  showDivs2(slideIndex2);
+
+  function currentDiv2(n) {
+    showDivs2(slideIndex2 = n);
+  }
+
+  function showDivs2(n) {
+    var i;
+    var x = document.getElementsByClassName("slider-second__item");
+    var dots = document.getElementsByClassName("slider-second__switch");
+    if (n > x.length) {slideIndex2 = 1}
+    if (n < 1) {slideIndex2 = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].classList.add("slider-second__item--display");
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" slider-second__switch--active", "");
+    }
+    x[slideIndex2-1].className = x[slideIndex2-1].className.replace(" slider-second__item--display", "");
+    dots[slideIndex2-1].className += " slider-second__switch--active";
+  }
+
+  var sliderSecond = document.querySelector(".slider-second");
+  var buletSecond = sliderSecond.querySelectorAll(".slider-second__switch");
+  for (let i = 0; i < buletSecond.length; i++) {
+    buletSecond[i].onclick = function () {
+      currentDiv2(i+1);
+    };
+  }
+  var sliderSecondLeftArrow = sliderSecond.querySelector(".slider-second__main-switch--left");
+
+  var sliderSecondRightArrow = sliderSecond.querySelector(".slider-second__main-switch--right");
+
+  sliderSecondRightArrow.onclick = function () {
+    if (slideIndex2 > 0) {currentDiv2(slideIndex2-1);}
+    else {slideIndex2 = 4};
+
   };
 
-  bulet[2].onclick = function () {
-    currentDiv(3);
+  sliderSecondLeftArrow.onclick =function () {
+    if (slideIndex2 < 4) {currentDiv2(slideIndex2+1);}
+    else {slideIndex2 = 0};
   };
+
 
   function myLoop () {
     //  create a loop function
